ac898667b4a3c7ba664ef68636cfebd770ade5eb
Yeongjin-heo <ridi.heo@ridi.com>
2026-04-10 21:56:12 +0900

    260410 tailscale 싱태 연동

---
+++ codebackup/test_mweb/260410_tailscale 코드추가/tests/test_basic.py
@@ -0,0 +1,141 @@
+import pytest
+from config.settings import *
+from tests.elements import *
+from utils.helpers import *
+from selenium.common.exceptions import TimeoutException
+
+pytestmark = [
+    pytest.mark.aos,
+    pytest.mark.ios,
+    pytest.mark.real,
+    pytest.mark.emulator,
+    pytest.mark.simulator,
+    pytest.mark.chrome,
+    pytest.mark.samsung,
+    pytest.mark.safari,
+]
+
+class TestLogin:
+    @pytest.mark.order(1)
+    def test_login(self, logged_in): 
+        assert "account/login" not in logged_in.current_url, \
+            f"❌ 로그인 실패. 현재 URL: {logged_in.current_url}"
+        
+class TestContenthomeAll:
+    """ 회차목록 > 4화 대여결제 및 뷰어진입 """
+    @pytest.mark.order(2)
+    def test_contenthome_all(self, logged_in): 
+        driver = logged_in
+
+        contents_home_all = URLs.CONTENT_HOME_ALL_AOS if is_android(driver) else URLs.CONTENT_HOME_ALL_IOS
+        driver.get(contents_home_all)
+
+        btn_rent = wait_for_element_clickable(driver, ContentHome.RENT_BTN)
+        scroll_to_center_element(driver, btn_rent)
+        wait_seconds(3)
+
+        tap_element(driver, btn_rent)
+        if is_chrome(driver):
+            wait_for_element_visible(driver, PaymentPopup.FREE_VIEW_HEADER)
+            payment_method_btn = wait_for_element_clickable(driver, PaymentPopup.PAYMENT_METHOD_BTN)
+            tap_element(driver, payment_method_btn)
+
+        wait_for_element_visible(driver, PaymentPopup.PAYMENT_HEADER)
+        
+        btn_payment = wait_for_element_clickable(driver, PaymentPopup.PAYMENT_BTN)
+        tap_element(driver, btn_payment)
+
+        viewer_contents_all = URLs.CONTENT_ALL_VIEW_AOS if is_android(driver) else URLs.CONTENT_ALL_VIEW_IOS
+        wait_for_url_contains(driver, viewer_contents_all)
+        assert viewer_contents_all in driver.current_url, \
+            f"❌ 뷰어 진입 실패"
+
+class TestViewerAll:
+    """ 뷰어진입 및 다음화 결제 """
+    @pytest.mark.order(3)
+    def test_viewer_all(self, logged_in): 
+        driver = logged_in
+
+        viewer_contents_all = URLs.CONTENT_ALL_VIEW_AOS if is_android(driver) else URLs.CONTENT_ALL_VIEW_IOS
+        driver.get(viewer_contents_all)
+
+        btn_next= wait_for_element_clickable(driver, Viewer.NEXT_BTN)
+        tap_element(driver, btn_next)
+        wait_for_element_visible(driver, PaymentPopup.SELECT_PAYMENT_HEADER)
+        
+        btn_rent_nextcontent = wait_for_element_clickable(driver, PaymentPopup.RENT_NEXT_BTN)
+        tap_element(driver, btn_rent_nextcontent)
+        if is_chrome(driver):
+            wait_for_element_visible(driver, PaymentPopup.FREE_VIEW_HEADER)
+            next_payment_method_btn = wait_for_element_clickable(driver, PaymentPopup.NEXT_PAYMENT_METHOD_BTN)
+            tap_element(driver, next_payment_method_btn)
+        wait_for_element_visible(driver, PaymentPopup.PAYMENT_HEADER)
+        
+        btn_next_payment = wait_for_element_clickable(driver, PaymentPopup.NEXT_PAYMENT_BTN)
+        tap_element(driver, btn_next_payment)
+        
+        viewer_nextcontents_all = URLs.CONTENT_ALL_NEXTVIEW_AOS if is_android(driver) else URLs.CONTENT_ALL_NEXTVIEW_IOS
+        wait_for_url_contains(driver, viewer_nextcontents_all)
+        assert viewer_nextcontents_all in driver.current_url, \
+            f"❌ 다음화 뷰어 진입 실패"
+
+class TestCart:
+    """ 회차목록 > 카트담기 """
+    @pytest.mark.order(4)
+    def test_cart(self, logged_in):
+        driver = logged_in
+        driver.get(URLs.CONTENT_HOME)
+
+        select_area = wait_for_element_visible(driver, ContentHome.SELECT_ALL)
+        scroll_to_center_element(driver, select_area)
+        wait_seconds(3)
+        
+        checkbox_first = wait_for_element(driver, ContentHome.FIRST_EPISODE_CHECKBOX)
+        tap_element(driver, checkbox_first)
+
+        btn_cart = wait_for_element_clickable(driver, ContentHome.CART_BTN)
+        tap_element(driver, btn_cart)
+
+        try:
+            toast_msg = wait_for_element_visible(driver, ContentHome.TOAST_MSG_CART, timeout=5)
+            assert toast_msg.is_displayed(), "❌ 카트담기 실패"
+        except TimeoutException:
+            assert False, "❌ 카트담기 토스트 메시지 미노출"
+        
+class TestCartPyament:
+    """ 카트 소장탭 > 작품 구매 """
+    @pytest.mark.order(5)
+    def test_cartpage(self, logged_in):
+        driver = logged_in
+        driver.get(URLs.CART)
+
+        buy_tab = wait_for_element(driver, CartPage.BUY_TAB)
+        if "selected" in buy_tab.get_attribute("class"):
+            print("소장가능 탭 활성화 상태")
+        else:
+            tap_element(driver, buy_tab)
+            wait_for_element_visible(driver, CartPage.BUY_TAB_SELECTED)
+            print("소장가능 탭 선택 및 활성화")
+        assert "selected" in buy_tab.get_attribute("class"), \
+            "소장가능 탭이 활성화되지 않았습니다."
+
+        checkbox_all = wait_for_element_clickable(driver, CartPage.SELECT_ALL_CHECKBOX)
+        tap_element(driver, checkbox_all)
+
+        checkbox_first = wait_for_element(driver, CartPage.FIRST_CONTENTS_CHECKBOX)
+        tap_element(driver, checkbox_first)
+
+        btn_buy = wait_for_element_clickable(driver, CartPage.BUY_BTN)
+        tap_element(driver, btn_buy)
+
+        wait_for_url_contains(driver, URLs.CHECKPOUT)
+        assert URLs.CHECKPOUT in driver.current_url, \
+            f"❌ 결제페이지 진입 실패"
+
+        togle_agree_payment = wait_for_element_clickable(driver, CheckoutPage.PAYMENT_AGREE_TOGGLE)
+        btn_payment = wait_for_element_clickable(driver, CheckoutPage.PAYMENT_BTN)
+        tap_element(driver, togle_agree_payment)
+        tap_element(driver, btn_payment)
+
+        complete_payment = wait_for_element_visible(driver, CheckoutPage.PAYMENT_COMPLETE)
+        assert complete_payment.is_displayed(), "❌ 카트 결제 실패"
